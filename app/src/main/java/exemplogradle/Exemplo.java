f42af6b6a3a33d13249fe9d62ae4697fb1119f55
Mafer Zanguetim <mafer.zanguetimcontatos@gmail.com>
2023-09-11 22:50:38 +0000

    Segunda aula de java

---
+++ app/src/main/java/exemplogradle/Exemplo.java
@@ -0,0 +1,9 @@
+package exemplogradle; 
+//configuração dos pacotes - 1°
+
+public class Exemplo{
+
+    public static void main(String[] args) {
+        System.out.println("Olá Gradle");
+    }
+}
